079bc3ee6802c39f71a5f854e4fdca4f6af07255
Ing. Petr Jindra <el.mordo@gmail.com>
2017-10-09 07:44:53 +0200

    initial commit

---
+++ src/IRenderer.ts
@@ -0,0 +1,10 @@
+
+import { DataObject } from "./DataObject"
+
+export class IRenderer {
+
+    public render(data: DataObject) {
+        throw new Error("Not implemented");
+    }
+
+}
---
+++ src/RenderedContent.ts
@@ -0,0 +1,14 @@
+
+import { CommonHtmlElement } from "./CommonHtmlElement";
+
+export class RenderedContent {
+
+    public getContent() {
+        throw new Error("Not implemented");
+    }
+
+    public getNamedElement(name: string) {
+        throw new Error("Not implemented");
+    }
+
+}
