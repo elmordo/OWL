9200c1633a9e1a6da803e31f3a1a5e9e2a3b223e
Ing. Petr Jindra <el.mordo@gmail.com>
2017-10-10 10:45:16 +0200

    dom manipulator update

--- src/dom.ts
+++ src/dom.ts
@@ -7,7 +7,7 @@ export namespace OOW {
 
         readonly rootElement: CommonHtmlElement;
 
-        private _cache: ElementCache;
+        private _cache: MappedElementCache;
 
         constructor (window: Window, rootElement: HTMLElement) {
             this.window = window;
@@ -35,6 +35,132 @@ export namespace OOW {
         return manipulator;
     }
 
+
+    /**
+     * contains <cache id>-<mapped element> pairs
+     */
+    class MappedElementLookup {
+
+        [key: number]: CommonHtmlElement;
+
+    }
+
+    /**
+     * hold already mapped elements
+     */
+    class MappedElementCache {
+
+        /**
+         * attribute name where OOW internal id is stored
+         * @type {String}
+         */
+        public static ELEMENT_INTERNAL_ID_NAME = "oow-id";
+
+        /**
+         * id of next cached element
+         * @type {number}
+         */
+        private static _nextId: number;
+
+        /**
+         * container with cached elements
+         * @type {MappedElementLookup}
+         */
+        private _cache: MappedElementLookup;
+
+        /**
+         * parent document
+         * @type {Document}
+         */
+        readonly document: Document;
+
+        /**
+         * initialize instance
+         */
+        constructor() {
+            this._cache = new MappedElementLookup();
+        }
+
+        /**
+         * is element cached?
+         * @param {HTMLElement} element element to test
+         * @return {boolean} true if element is cached, false otherwise
+         */
+        public isCached(element: HTMLElement) : boolean {
+            return this._hasElementId(element);
+        }
+
+        /**
+         * add element to the cache
+         * @param {CommonHtmlElement} mappedElement mapped element to add
+         */
+        public addElement(mappedElement: CommonHtmlElement) : void {
+            let id:number = this._setElementId(mappedElement.element);
+            this._cache[id] = mappedElement;
+        }
+
+        /**
+         * get mapped element from the cache
+         * @param {HTMLElement} element raw HTML element
+         * @return {CommonHtmlElement} cached element
+         * @throws Error element is not cached
+         */
+        public getCached(element: HTMLElement) : CommonHtmlElement {
+            let id: number = this._getElementId(element);
+            return this._cache[id];
+        }
+
+        /**
+         * remove element from the cache
+         * @param {HTMLElement} element raw HTML element to remove
+         * @throws Error element is not in cache
+         */
+        public removeFromCache(element: HTMLElement) : void {
+            let id: number = this._getElementId(element);
+            delete this._cache[id];
+        }
+
+        /**
+         * test if element has cache id
+         * @param {HTMLElement} element raw HTML element to test
+         * @return {boolean} true if element has cache id
+         */
+        private _hasElementId(element: HTMLElement) : boolean {
+            return element.attributes.getNamedItem(MappedElementCache.ELEMENT_INTERNAL_ID_NAME) !== null;
+        }
+
+        /**
+         * get element cache id
+         * @param {HTMLElement} element raw HTML element
+         * @return {number} element's cache id
+         * @throws Error element has no cache id
+         */
+        private _getElementId(element: HTMLElement) : number {
+            let attr: Attr = element.attributes.getNamedItem(MappedElementCache.ELEMENT_INTERNAL_ID_NAME);
+
+            if (attr === null) throw new Error("Element is not cached");
+
+            return Number(attr.value);
+        }
+
+        /**
+         * set new cache id to the element
+         * @param {HTMLElement} element raw HTML element
+         * @return {number} new assigned cache id
+         */
+        private _setElementId(element: HTMLElement) : number {
+            let id: number = MappedElementCache._nextId++;
+
+            let attr: Attr = document.createAttribute(MappedElementCache.ELEMENT_INTERNAL_ID_NAME);
+            attr.value = id.toString();
+            element.attributes.setNamedItem(attr);
+
+            return id;
+        }
+
+    }
+
+
     class AttributeCache {
 
         [key: string]: Attribute;
@@ -55,9 +181,5 @@ export namespace OOW {
         }
 
     };
-
-    export class Attribute {
-        H
-    }
 
 }
