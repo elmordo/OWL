1b4377a8705f8ea77ae041679ff6bdb11f823965
Ing. Petr Jindra <el.mordo@gmail.com>
2017-10-13 10:41:04 +0200

    unit tests

---
+++ tests/unittests/events/test_event_handler.ts
@@ -0,0 +1,15 @@
+import { OOW } from '../../../src/events';
+
+type EventDispatcher = OOW.EventDispatcher
+
+import { expect } from 'chai';
+// if you used the '@types/mocha' method to install mocha type definitions, uncomment the following line
+// import 'mocha';
+
+
+describe('Hello function', () => {
+  it('should return hello world', () => {
+
+  });
+});
+
