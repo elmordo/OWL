4bb3ee4be0c4282725dc343bb32ef8d9ac05452a
Ing. Petr Jindra <el.mordo@gmail.com>
2017-10-20 15:04:44 +0200

    sizer factories

--- src/view/sizer/base.ts
+++ src/view/sizer/base.ts
@@ -1,59 +1,193 @@
 import { CommonHtmlNode, CommonHtmlElement } from "../../dom"
 
 
+/**
+ * common sizer interface
+ */
 export interface ISizer {
 
-    updateSize(node: CommonHtmlNode, options: Object): void;
+    /**
+     * update size of the sized element
+     */
+    updateSize(): void;
+
+    /**
+     * setup instance
+     * @param {CommonHtmlNode} node node to manage
+     * @param {Object} options options
+     */
+    setup(node: CommonHtmlNode, options: Object): void;
+
+    /**
+     * tear down the sizer (unbind event listeners, etc)
+     */
+    teardown(): void;
 
 }
 
 
+/**
+ * manage available types of the sizers
+ */
 export class SizerManager {
 
+    /**
+     * set of sizers
+     * @type {SizerLookup}
+     */
     private _sizers: SizerLookup;
 
+    /**
+     * initialize instance
+     */
     constructor() {
         this._sizers = new SizerLookup();
     }
 
-    public addSizer(name: string, sizer: ISizer) : void {
+    /**
+     * register sizer to the manager
+     * @param {string} name name of the sizer
+     * @param {Function} factory sizer factory function
+     */
+    public addSizer(name: string, factory: Function) : void {
         if (this._sizers[name])
             throw new Error("Sizer '" + name + "' does exist.");
-        this._sizers[name] = sizer;
+        this._sizers[name] = factory;
     }
 
+    /**
+     * get sizer instance
+     * @param {string} name name of the sizer
+     * @return {ISizer} sizer instance
+     */
     public getSizer(name: string) : ISizer {
         if (!this._sizers[name])
             throw new Error("Sizer '" + name + "' does not exist.");
 
-        return this._sizers[name];
+        return this._sizers[name]();
     }
 }
 
 
-export class FitParent {
+/**
+ * the abstract sizer with common shared functionality
+ */
+export abstract class ASizer implements ISizer {
 
-    public updateSize(node: CommonHtmlNode, options: Object): void {
-        let parent: HTMLElement = node.node.parentElement;
-        let element = <CommonHtmlElement>node;
+    /**
+     * node to size
+     * @type {CommonHtmlNode}
+     */
+    protected _node: CommonHtmlNode;
+
+    /**
+     * sizer options
+     * @type {Object}
+     */
+    protected _options: Object;
+
+    /**
+     * update size of the node
+     */
+    public abstract updateSize(): void;
+
+    /**
+     * setup the instance
+     * @param {CommonHtmlNode} node node to size
+     * @param {Object} options options
+     */
+    public setup(node: CommonHtmlNode, options: Object): void {
+        this._node = node;
+        this._options = options;
+    }
+
+    public teardown() : void {
+        // do nothing
+    }
+}
+
+
+/**
+ * fit element to parent's dimension
+ */
+export class FitParent extends ASizer {
+
+    /**
+     * callback for resize event
+     * @type {Function}
+     */
+    private _callback;
+
+    /**
+     * update element size
+     */
+    public updateSize(): void {
+        let parent: HTMLElement = this._node.node.parentElement;
+        let element = <CommonHtmlElement>this._node;
 
         let styles = element.styles;
         styles.set("width", parent.offsetWidth + "px");
         styles.set("height", parent.offsetHeight + "px");
     }
+
+    /**
+     * setup instance
+     * @param {CommonHtmlNode} node node to manage
+     * @param {Object} options options
+     */
+    public setup(node: CommonHtmlNode, options: Object) : void {
+        super.setup(node, options);
+        this._callback = () => { this.updateSize() };
+
+        window.addEventListener("resize", this._callback);
+    }
+
+    /**
+     * remove registered event listeners
+     */
+    public teardown() : void {
+        window.removeEventListener("resize", this._callback);
+    }
 }
 
 
-export class FitWindow {
+export class FitWindow extends ASizer {
 
-    public updateSize(node: CommonHtmlNode, options: Object): void {
-        let element = <CommonHtmlElement>node;
+    /**
+     * callback for resize event
+     * @type {Function}
+     */
+    private _callback;
+    /**
+     * update element size
+     */
+    public updateSize(): void {
+        let element = <CommonHtmlElement>this._node;
         element.styles.set("width", window.innerWidth + "px");
         element.styles.set("height", window.innerHeight + "px");
+    }
+
+    /**
+     * setup instance
+     * @param {CommonHtmlNode} node node to manage
+     * @param {Object} options options
+     */
+    public setup(node: CommonHtmlNode, options: Object) : void {
+        super.setup(node, options);
+        this._callback = () => { this.updateSize() };
+
+        window.addEventListener("resize", this._callback);
+    }
+
+    /**
+     * remove registered event listeners
+     */
+    public teardown() : void {
+        window.removeEventListener("resize", this._callback);
     }
 }
 
 
 class SizerLookup {
-    [key: string]: ISizer;
+    [key: string]: Function;
 }
--- src/view/sizer/factory.ts
+++ src/view/sizer/factory.ts
@@ -3,8 +3,8 @@ import { SizerManager, FitParent, FitWindow } from "./base"
 export function factory(): SizerManager {
     let manager = new SizerManager();
 
-    manager.addSizer("fitParent", new FitParent());
-    manager.addSizer("fitWindow", new FitWindow());
+    manager.addSizer("fitParent", () => { return new FitParent(); });
+    manager.addSizer("fitWindow", () => { return new FitWindow(); });
 
     return manager;
 }
